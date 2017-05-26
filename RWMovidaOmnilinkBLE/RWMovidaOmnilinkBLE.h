a7b3e06c62875575a17c531af49d1fa6e07e3ada
Joao Felipe <joao.matos@e-deploy.com.br>
2017-05-26 15:18:57 -0300

    Inicial Commit

---
+++ RWMovidaOmnilinkBLE/RWMovidaOmnilinkBLE.h
@@ -0,0 +1,19 @@
+//
+//  RWMovidaOmnilinkBLE.h
+//  RWMovidaOmnilinkBLE
+//
+//  Created by Joao Felipe on 5/26/17.
+//  Copyright © 2017 João Felipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWMovidaOmnilinkBLE.
+FOUNDATION_EXPORT double RWMovidaOmnilinkBLEVersionNumber;
+
+//! Project version string for RWMovidaOmnilinkBLE.
+FOUNDATION_EXPORT const unsigned char RWMovidaOmnilinkBLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWMovidaOmnilinkBLE/PublicHeader.h>
+
+
